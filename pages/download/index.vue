35210c3d426efb41af58819b5f647808bfd68f45
Seth <seth.mkline@gmail.com>
2021-05-16 11:56:11 -0400

    adds downloads

--- pages/download/index.vue
+++ pages/download/index.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="container">Download</div>
+  <div class="container">
+    <h1 class="title">Downloads</h1>
+    <table v-if="downloads && downloads.length" class="table">
+      <thead>
+        <tr>
+          <td>Name</td>
+          <td>Description</td>
+          <td>Size</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="download in downloads" :key="download.id">
+          <td>
+            <a :href="download.link" download>{{ download.name }}</a>
+          </td>
+          <td>{{ download.description }}</td>
+          <td>{{ download.size }}MB</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>There are no downloads right now check back later</p>
+  </div>
 </template>
 
 <script>
 export default {
   middleware: 'auth',
-  name: 'download',
+  name: 'Download',
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('/downloads')
+    return { downloads: data }
+  },
 }
 </script>
